cef372a7d8ca55713ede752c5f0e49dd828c5bf4
duongviethuy <duonghuyboy@gmail.com>
2021-09-08 21:40:56 +0700

    add new function

--- Tuan01/HelloWorld/src/App.java
+++ Tuan01/HelloWorld/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int age;
+        String name;
         System.out.println("Hello, World!");
+        System.out.println("Input your name: ");
+        name = sc.nextLine();
+        System.out.println("Input you age: ");
+        age = sc.nextInt();
+        if (age < 18)
+            System.out.println("Sorry you don't grow enough");
+        else
+            System.out.println("Hello " + name);
     }
 }
