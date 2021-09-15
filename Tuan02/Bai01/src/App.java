6bbfc6ee271a3257e12c3a15548e92fdf72d4374
duongviethuy <duonghuyboy@gmail.com>
2021-09-15 12:46:42 +0700

    addEx1

---
+++ Tuan02/Bai01/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String inputString = sc.nextLine();
+        System.out.println("Your output: " + inputString.toUpperCase());       
+
+    }
+}
