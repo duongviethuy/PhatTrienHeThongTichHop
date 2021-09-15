a94eecb71326f10ec2e08385c8f3e8e32ba01fa6
duongviethuy <duonghuyboy@gmail.com>
2021-09-15 13:01:18 +0700

    Bai02

--- Tuan02/Bai02/src/App.java
+++ Tuan02/Bai02/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
+    public static Scanner sc = new Scanner(System.in);    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Program check Prime Number");
+        System.out.print("Input number that you need to check: ");
+        int checkPrNum = sc.nextInt();
+        int count = 1;
+        for(int i=1 ;i<checkPrNum;i++) {
+            if(checkPrNum % i != 0 ) {
+                count ++;
+            }
+        }
+        if (count>1) {
+            System.out.println("It isn't a Prime Number");
+        } else System.out.println("It is a Prime Number");
+
     }
 }
